f7a996ee4a12a32a23062d0ee155d0064dd1f213
lukmak0394 <lukmak0394@wp.pl>
2022-01-02 17:29:46 +0100

    form validation

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 window.addEventListener('scroll',navColorChange);
-
 const navBar = document.querySelector('.navbar');
 
+document.querySelector('#name').value = '';
+document.querySelector('#email').value = '';
+document.querySelector('#phone').value = '';
+document.querySelector('#message').value = '';
+
+let globalName = document.querySelector('#name');
+let globalEmail = document.querySelector('#email');
+
+const form = document.querySelector('.my-form');
+form.addEventListener('submit',validate);
+
+
 function navColorChange() {
-    
 
     // Check if window is scrolled more than 300px from top
     if (window.scrollY >= 250) {
@@ -16,3 +26,47 @@ function navColorChange() {
         navBar.classList.remove('bg-primary');
     };
 };
+
+
+function eventListeners() {
+    globalName.addEventListener('focus',function() {
+        globalName.style.outline = '2px solid #0d6efd';
+        document.querySelector('.nameError').classList.add('d-none');
+    }) 
+    globalName.addEventListener('blur',function(){
+        globalName.style.outline = 'none';
+    })
+    globalEmail.addEventListener('focus',function() {
+        globalEmail.style.outline = '2px solid #0d6efd';
+        document.querySelector('.mailError').classList.add('d-none');
+    }) 
+    globalEmail.addEventListener('blur',function(){
+        globalEmail.style.outline = 'none';
+    })
+}
+
+
+function validate(event) {
+    const name = document.querySelector('#name');
+    const email = document.querySelector('#email');
+
+    if (name.value.trim() === '') {
+        event.preventDefault();
+        name.style.outline = "thick solid red";
+        document.querySelector('.nameError').classList.remove('d-none')
+    } 
+   
+    if (email.value.trim() === '') {
+        event.preventDefault();
+        email.style.outline = "thick solid red"; 
+        document.querySelector('.mailError').classList.remove('d-none')
+    } 
+
+    eventListeners();
+
+    document.querySelector('#name').value = '';
+    document.querySelector('#email').value = '';
+    document.querySelector('#phone').value = '';
+    document.querySelector('#message').value = '';
+}
+
