2560d2e7cc7b61ef7c752be5b9dfa730586423ef
lukmak0394 <lukmak0394@wp.pl>
2022-01-02 18:11:27 +0100

    test

--- main.js
+++ main.js
@@ -13,14 +13,15 @@ const form = document.querySelector('.my-form');
 form.addEventListener('submit',validate);
 
 
-function navColorChange() {
 
+function navColorChange() {
     // Check if window is scrolled more than 300px from top
     if (window.scrollY >= 250) {
         // If yes - check if navbar contains class nav-opacity. Result of this if will be no casue this class hasn't been set by default.
         if (navBar.classList.contains('bg-primary'));
         // Add this class to navbar
         navBar.classList.add('bg-primary');
+        
     } else {
         // If scroll Y < 300px , remove class that was added when we scrolled down
         navBar.classList.remove('bg-primary');
